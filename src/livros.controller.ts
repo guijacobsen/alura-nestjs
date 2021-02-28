15cf3805a9d26d9a1b181c1b540e09cd6aacea48
Guilherme Jacobsen <guijacobsen@gmail.com>
2021-02-28 15:26:31 -0300

    WIP: Aula 4 - Refatorando código - alterando produtos -> livros

---
+++ src/livros.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+import { Livro } from './livro.model';
+import { LivrosService } from './livros.service';
+
+@Controller('livros')
+export class LivrosController {
+  constructor(private livrosService: LivrosService) {}
+
+  @Get()
+  getAll(): Livro[] {
+    return this.livrosService.getAll();
+  }
+
+  @Get(':id')
+  get(@Param() params): Livro {
+    return this.livrosService.get(params.id);
+  }
+
+  @Post()
+  store(@Body() body: Livro) {
+    const produto = { id: Math.random(), ...body };
+    return this.livrosService.store(produto);
+  }
+
+  @Put()
+  update(@Body() body: Livro): Livro {
+    return this.livrosService.update(body);
+  }
+
+  @Delete(':id')
+  delete(@Param() params) {
+    this.livrosService.delete(params.id);
+  }
+}
---
+++ src/livros.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { Livro } from './livro.model';
+
+@Injectable()
+export class LivrosService {
+  livros: Livro[] = [
+    new Livro('LIV01', 'Livro TDD e BDD na prática', 29.9),
+    new Livro('LIV02', 'Livro iniciando com Flutter', 39.9),
+    new Livro('LIV03', 'Inteligência artificial como serviço', 29.9),
+  ];
+
+  getAll(): Livro[] {
+    return this.livros;
+  }
+
+  get(id: number): Livro {
+    return this.livros[0];
+  }
+
+  store(livro: Livro) {
+    this.livros.push(livro);
+  }
+
+  update(livro: Livro) {
+    return livro;
+  }
+
+  delete(id: number) {
+    this.livros.pop();
+  }
+}
